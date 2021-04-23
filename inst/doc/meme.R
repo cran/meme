23940aa5461a63d68e766c637450eb9201872cc5
Guangchuang Yu <guangchuangyu@gmail.com>
2021-04-23 09:00:02 +0000

    version 0.2.3

--- inst/doc/meme.R
+++ inst/doc/meme.R
@@ -1,4 +1,4 @@
-## ----include=FALSE-------------------------------------------------------
+## ----include=FALSE------------------------------------------------------------
 knitr::opts_chunk$set(warning = FALSE,
                       message = TRUE)
 
@@ -8,7 +8,7 @@ library(ggimage)
 library(meme)
 library(cowplot)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 if (.Platform$OS.type == "windows") {
     windowsFonts(
         Impact = windowsFont("Impact"),
@@ -16,41 +16,41 @@ if (.Platform$OS.type == "windows") {
     )
 }
 
-## ----fig.width=7, fig.height=3.9375--------------------------------------
+## ----fig.width=7, fig.height=3.9375-------------------------------------------
 library(meme)
 u <- system.file("angry8.jpg", package="meme")
 meme(u, "code", "all the things!")
 
-## ----fig.width=7, fig.height=3.9375--------------------------------------
+## ----fig.width=7, fig.height=3.9375-------------------------------------------
 mmplot(u) + mm_caption("calm down", "and RTFM",
                        color="purple")
 
-## ----message=TRUE--------------------------------------------------------
+## ----message=TRUE-------------------------------------------------------------
 u2 <- system.file("success.jpg", package="meme")
 x <- meme(u2, "please", "tell me more")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  outfile <- tempfile(fileext=".png")
 #  meme_save(x, file=outfile)
 
-## ----fig.width=7, fig.height = 3.94--------------------------------------
+## ----fig.width=7, fig.height = 3.94-------------------------------------------
 plot(x, size = 2, "happy friday!", "wait, sorry, it's monday", color = "firebrick", font = "Courier")
 
-## ----fig.width=7, fig.height = 3.94--------------------------------------
+## ----fig.width=7, fig.height = 3.94-------------------------------------------
 x + aes(upper = "#barbarplots",
         lower = "friends don't let friends make bar plots",
         color = firebrick, font = Courier, size=1.5)
 
-## ----fig.width=7, fig.height = 3.94, eval=FALSE--------------------------
+## ----fig.width=7, fig.height = 3.94, eval=FALSE-------------------------------
 #  x + list(upper = "#barbarplots",
 #          lower = "friends don't let friends make bar plots",
 #          color = "firebrick", font = "Courier", size=1.5)
 
-## ----fig.width=7, fig.height=3.9375--------------------------------------
+## ----fig.width=7, fig.height=3.9375-------------------------------------------
 y <- meme(u, "卧槽", "听说你想用中文", font="STHeiti")
 y
 
-## ----fig.width=7---------------------------------------------------------
+## ----fig.width=7--------------------------------------------------------------
 library(grid)
 mm <- meme(u, "code", "all the things!", size=.3, color='firebrick', bgcolor=NULL)
 
@@ -66,7 +66,7 @@ for (i in seq_along(xx)) {
     print(mm, vp = vp)
 }
 
-## ----fig.width=7, fig.height=4.06----------------------------------------
+## ----fig.width=7, fig.height=4.06---------------------------------------------
 library(ggplot2)
 library(ggimage)
 
@@ -78,11 +78,11 @@ ggplot(d, aes(x, y)) +
     geom_subview(x = 0, y = 0, subview=mm+aes(size=3), width=Inf, height=Inf) +
     geom_point() + geom_line()
 
-## ----fig.width=7, fig.height=7.88, eval=F--------------------------------
+## ----fig.width=7, fig.height=7.88, eval=F-------------------------------------
 #  library(cowplot)
 #  plot_grid(x, y, ncol=1, labels = c("A", "B"))
 
-## ----fig.width=8, fig.height=4.5, fig.showtext=TRUE, message=TRUE--------
+## ----fig.width=8, fig.height=4.5, fig.showtext=TRUE, message=TRUE-------------
 ## import pokemon fonts
 ## downloaded from <https://fontmeme.com/fonts/pokmon-font/>
 font_pokemon()
@@ -93,7 +93,7 @@ u <- system.file("ash-pikachu.0.0.jpg", package="meme")
 meme(u, "Pokemon", "pikachu i choose you!", font='Pokemon_Hollow')
 meme(u, "Pokemon", "pikachu i choose you!", font='Pokemon_Solid', color='#FCCF00')
 
-## ----fig.width=7, fig.height=3.5, fig.showtext=TRUE, message=TRUE--------
+## ----fig.width=7, fig.height=3.5, fig.showtext=TRUE, message=TRUE-------------
 ## folder that contains bubble1 font
 ## downloaded from https://fontmeme.com/fonts/bubble-1-font/
 dir <- system.file('fonts/bubble', package='meme')
@@ -101,7 +101,7 @@ font_import(dir)
 
 meme(u, "the meme package", "is awesome!", font="bubble1")
 
-## ----fig.width=7, fig.height=3.5, fig.showtext=TRUE, message=TRUE--------
+## ----fig.width=7, fig.height=3.5, fig.showtext=TRUE, message=TRUE-------------
 qplot(1:10, 1:10) + labs(title="meme is awesome") +
     theme(plot.title=element_text(family='bubble1', size=30, color='firebrick'))
 
